e2de2c7bbd029473a86726e18a0a18617d8d8cd8
bfrpaulondev <bfrpaulondev@gmail.com>
2025-02-12 21:05:22 +0000

    Add database.js

---
+++ config/database.js
@@ -0,0 +1,11 @@
+// config/database.js
+const mongoose = require('mongoose');
+
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/wiki-api';
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Conectado ao MongoDB'))
+  .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
