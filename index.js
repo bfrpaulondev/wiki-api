0287b41985e528497900417bde999ea17350e3ef
Bruno Paulon <143882052+bfrpaulondev@users.noreply.github.com>
2025-02-14 09:26:24 +0000

    created index.html page

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 const express = require('express');
 const path = require('path');
 const autoRoutes = require('./routes/autoRoutes');
@@ -10,51 +9,51 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 const cors = require('cors');
-const allowedOrigin = process.env.ALLOWED_ORIGIN || '*'; // You can set ALLOWED_ORIGIN in your .env file
-app.use(cors({
-  origin: allowedOrigin
-}));
+
+const allowedOrigin = process.env.ALLOWED_ORIGIN || '*';
+app.use(cors({ origin: allowedOrigin }));
 
 // Conexão com o banco de dados
 require('./config/database');
 
+// Configurar Swagger
 const swaggerOptions = {
     definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'Wiki API',
-        version: '1.0.0',
-        description: 'API para gerenciamento de Wiki',
-      },
-      servers: [
-        {
-         // url: 'http://localhost:' + (process.env.PORT || 3000) + '/api',
-          url: 'https://wiki-api-glte.onrender.com/' + 'api/',
-          description: 'Servidor de desenvolvimento',
-
+        openapi: '3.0.0',
+        info: {
+            title: 'Wiki API',
+            version: '1.0.0',
+            description: 'API para gerenciamento de Wiki',
         },
-      ],
-      components: {
-        securitySchemes: {
-          bearerAuth: {
-            type: 'http',
-            scheme: 'bearer',
-            bearerFormat: 'JWT',
-          },
+        servers: [
+            {
+                url: 'https://wiki-api-glte.onrender.com/api/',
+                description: 'Servidor de desenvolvimento',
+            },
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                },
+            },
         },
-      },
-      security: [
-        {
-          bearerAuth: [],
-        },
-      ],
+        security: [{ bearerAuth: [] }],
     },
     apis: [path.join(__dirname, '/controllers/*.js'), path.join(__dirname, '/models/*.js')],
-  };
-  
+};
 
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Servir a página inicial
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Registra automaticamente os controllers via autoRoutes
 autoRoutes(app);
@@ -62,5 +61,5 @@ autoRoutes(app);
 // Inicia o servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
